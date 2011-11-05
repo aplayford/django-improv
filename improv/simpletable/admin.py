234b21ec5c8b45544c7879dad8daf70dbc8029c7
Jason Bartz <jrbartz@gmail.com>
2011-11-05 14:10:35 -0400

    remove extraneous tabs

--- improv/simpletable/admin.py
+++ improv/simpletable/admin.py
@@ -2,11 +2,11 @@ from django.contrib import admin
 from simpletable.models import SimpleTable, SimpleColumn
 
 class SimpleColumnInline(admin.StackedInline):
-	exclude = ('field',)
-	extra = 0
-	model = SimpleColumn
+    exclude = ('field',)
+    extra = 0
+    model = SimpleColumn
 
 class SimpleTableAdmin(admin.ModelAdmin):
-	inlines = [SimpleColumnInline]
+    inlines = [SimpleColumnInline]
 
 admin.site.register(SimpleTable, SimpleTableAdmin)
