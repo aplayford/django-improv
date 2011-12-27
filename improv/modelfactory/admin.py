c1f3882bfe3b967c64ec4fd6d3f14ed2cf640f53
Adam Playford <adam.playford@gmail.com>
2011-12-27 18:41:59 -0500

    Complete core functionality of admin page load_and_introspect_tsv.

--- improv/modelfactory/admin.py
+++ improv/modelfactory/admin.py
@@ -1,9 +1,10 @@
 from modelfactory.models import DynamicModel, DynamicField
-from modelfactory.forms import EasyLoadFileForm, EasyLoadTextForm
+from modelfactory.forms import EasyLoadFileForm, EasyLoadTextForm, EasyLoadFileForm
 
 from django.contrib import admin
 from django.conf.urls.defaults import patterns
 
+from django.http import HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template import RequestContext
 
@@ -20,28 +21,43 @@ class DynamicModelAdmin(admin.ModelAdmin):
     def get_urls(self):
         urls = super(DynamicModelAdmin, self).get_urls()
         my_urls = patterns('',
-            (r'load/csv/', self.load_and_introspect),
+            (r'load/csv/', self.load_and_introspect_csv),
+            (r'load/tsv/', self.load_and_introspect_tsv),
         )
         return my_urls + urls
-
-    def load_and_introspect(self, request):
-        from modelfactory.loaders import load_and_introspect_tsv
+        
+    def load_and_introspect_csv(self, request):
+        from modelfactory.loaders import load_and_introspect_csv
 
         if request.method == 'POST': # If the form has been submitted...
-            form = EasyLoadTextForm(request.POST) # A form bound to the POST data
+            form = EasyLoadFileForm(request.POST, request.FILES) # A form bound to the POST data
             if form.is_valid(): # All validation rules pass
-                try:
-                    dyn_mod = load_and_introspect_tsv(form.cleaned_data['tsv_data'], form.cleaned_data['model_name'], overwrite=True)
-                    return HttpResponseRedirect('/thanks/') # Redirect after POST
-                except:
-                    pass
+                dyn_mod = load_and_introspect_csv(request.FILES['csv_file'], form.cleaned_data['model_name'], overwrite=True)
+                return HttpResponseRedirect('/thanks/') # Redirect after POST
         else:
-            form = EasyLoadTextForm() # An unbound form
+            form = EasyLoadFileForm() # An unbound form
 
         return render_to_response('admin/modelfactory/dynamicmodel/loader.html', {
             'form': form,
         }, context_instance=RequestContext(request))
 
         load_and_introspect_csv(instance.file.path, instance.model_name, overwrite=True)
+
+
+    def load_and_introspect_tsv(self, request):
+        from modelfactory.loaders import Loader
+
+        if request.method == 'POST': # If the form has been submitted...
+            form = EasyLoadTextForm(request.POST) # A form bound to the POST data
+            if form.is_valid(): # All validation rules pass
+                loader = Loader()
+                dyn_mod = loader.load_and_introspect_tsv_text(form.cleaned_data['tsv_data'], form.cleaned_data['model_name'], overwrite=True)
+                return HttpResponseRedirect('/thanks/') # Redirect after POST
+        else:
+            form = EasyLoadTextForm() # An unbound form
+
+        return render_to_response('admin/modelfactory/dynamicmodel/loader.html', {
+            'form': form,
+        }, context_instance=RequestContext(request))
     
 admin.site.register(DynamicModel, DynamicModelAdmin)
