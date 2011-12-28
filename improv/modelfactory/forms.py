389d7054b686b065c510c536290455066cbc2002
Adam Playford <adam.playford@gmail.com>
2011-12-27 23:48:41 -0500

    Some basic cleanup of the upload form.
    Though there's more work to be done, this closes #3.

--- improv/modelfactory/forms.py
+++ improv/modelfactory/forms.py
@@ -1,5 +1,5 @@
 from django import forms
 
 class EasyLoadTextForm(forms.Form):
-    tsv_data = forms.CharField(widget=forms.widgets.Textarea())
     model_name = forms.SlugField(max_length=50)
+    tsv_data = forms.CharField(widget=forms.widgets.Textarea())
